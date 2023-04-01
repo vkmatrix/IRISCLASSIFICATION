aaa0f9dd92b592c87a22579c0546aa244ab2c3cd
Vaithiyanathan K <vkmatrixaditya@gmail.com>
2023-04-01 07:08:37 +0530

    c

--- app.py
+++ app.py
@@ -8,7 +8,7 @@ app=Flask(__name__)
 
 @app.route('/')
 def man():
-    return render_template('home.html')
+    return render_template('index.html')
 
 @app.route('/predict',methods=['POST'])
 def home():
